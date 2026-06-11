af56c4c3e3059768cf3164566ca6b2f13819841f
白河愁 <kryso@21cn.com>
2026-06-11 16:51:15 +0900

    Make desktop dialog translucent

--- src/buildInfo.js
+++ src/buildInfo.js
@@ -1,3 +1,3 @@
-export const BUILD_VERSION = "20260611.2";
+export const BUILD_VERSION = "20260611.3";
 export const BUILD_DATE = "20260611";
-export const BUILD_COUNT = 2;
+export const BUILD_COUNT = 3;
